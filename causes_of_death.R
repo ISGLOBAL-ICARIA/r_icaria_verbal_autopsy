b3c166091facd97cbacf60aeaed0e358f412525d
maxramirez84 <maxramirez84@gmail.com>
2021-10-19 12:25:50 +0200

    Export data from ODK Central server using the RESTful endpoint rather than OData.

--- causes_of_death.R
+++ causes_of_death.R
@@ -8,14 +8,26 @@ source("credentials.R")
 # Form 1.5.3 is setup
 kTimeZone <- "Africa/Freetown"
 ruODK::ru_setup(
-  svc     = kServiceURL, 
-  un      = kUsername, 
-  pw      = kPassword,
-  tz      = kTimeZone,
-  verbose = T
+  svc          = kServiceURL, 
+  un           = kUsername, 
+  pw           = kPassword,
+  tz           = kTimeZone,
+  verbose      = T,
+  odkc_version = 1.2
 )
 
-va.data <- ruODK::odata_submission_get()
+# As the OData export method has an opened bug 
+# (https://github.com/ropensci/ruODK/issues/100) which results on dropping empty
+# columns that need to be present anyway for CrossVA, we have to export the data
+# using the RESTful API. We download the data in a zip file and unpack it. Then
+# read the data into the csv file
+zip <- ruODK::submission_export()
+file <- unzip(zip)
+va.data <- read.csv(file, stringsAsFactors = F)
 
 # Ideally, we should only use approved records
-va.data <- va.data[which(va.data$system_review_state == "approved"), ]
+va.data <- va.data[which(va.data$ReviewState == "approved"), ]
+
+# Convert VAs using the odk2openVA() function for version 1.5.1+. We will be 
+# able to use either InterVA5 or insilico(data.type = "WHO2016") to assign CoD
+openva_input_v151 <- odk2openVA(va.data)
