16d91ceeae965f5c4dfd78353c8fa67b2ebe49c6
Andreu Bofill <andreu.bp@gmail.com>
2022-05-30 17:11:09 +0200

    URL variable in ODK setup

--- causes_of_death.R
+++ causes_of_death.R
@@ -8,18 +8,20 @@ source("credentials.R")
 kTimeZone <- "Africa/Freetown"
 kHIVMortality <- "l"     # h (high), l (low), or v (very low)
 kMalariaMortality <- "h" # h (high), l (low), or v (very low)
-
 # Extract data from the ODK Central server where the 2016 WHO Verbal Autopsy 
 # Form 1.5.3 is setup
+
 ruODK::ru_setup(
-  svc          = kServiceURL, 
+  #svc         = kServiceSVC
+  url          = kServiceURL,
+  pid          = kpid,
+  fid          = kfid,
   un           = kUsername, 
   pw           = kPassword,
   tz           = kTimeZone,
   verbose      = T,
-  odkc_version = 1.2
+  odkc_version = 1.4
 )
-
 # As the OData export method has an opened bug 
 # (https://github.com/ropensci/ruODK/issues/100) which results on dropping empty
 # columns that need to be present anyway for CrossVA, we have to export the data
@@ -32,8 +34,13 @@ va.data <- read.csv(file, stringsAsFactors = F)
 # Ideally, we should only use approved records
 va.data <- va.data[which(va.data$ReviewState == "approved"), ]
 
+## But we can analyze the ones not rejected
+#va.data <- va.data[which(va.data$ReviewState != "rejected"), ]
+
+
 # Convert VAs using the odk2openVA() function for version 1.5.1+. We will be 
 # able to use either InterVA5 or insilico(data.type = "WHO2016") to assign CoD
+
 openva_input_v151 <- odk2openVA(va.data)
 
 # Assign CoD with model = InterVA5 through codeVA
@@ -58,7 +65,6 @@ id.colums <- c(
 )
 va.data.ids <- va.data[, id.colums]
 colnames(va.data.ids) <- c("ID", "STUDYNUM")
-
 output <- merge(va.data.ids, output)
 
 kVersionFormat <- "%Y%m%d"
@@ -72,3 +78,4 @@ output.filename <- paste0(
   kCSVExtension
 )
 write.csv(output, file = output.filename, row.names = F)
+
